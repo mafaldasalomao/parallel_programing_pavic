7186c42e31aafc2cf6d6b5931dd23580577a7a7d
salomao-mafalda <salomaomachadon@gmail.com>
2022-10-05 09:50:24 -0500

    Fix value of max_pixel

--- parallel_programing/parallel_programing/parallel_programing.cpp
+++ parallel_programing/parallel_programing/parallel_programing.cpp
@@ -16,7 +16,7 @@ int main() {
 	//write body
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < heigth; j++) {
-			image << i % max_pixel << " " << j % max_pixel << " " << (i + j) % max_pixel << endl; // the 'mod' does not allow the value to exceed the value of max pixel
+			image << i % max_pixel+1 << " " << j % max_pixel+1 << " " << (i + j) % max_pixel+1 << endl; // the 'mod' does not allow the value to exceed the value of max pixel
 		}
 	}
 	//end body
