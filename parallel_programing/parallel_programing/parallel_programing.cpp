14c72fd323cfaea71f6cba95c4df9721624a1982
salomao-mafalda <salomaomachadon@gmail.com>
2022-10-05 10:05:35 -0500

    Create option random image

--- parallel_programing/parallel_programing/parallel_programing.cpp
+++ parallel_programing/parallel_programing/parallel_programing.cpp
@@ -1,6 +1,7 @@
-// import libs
 #include <iostream>
 #include <fstream> // to create file
+#include <stdlib.h>     /*if use srand, rand */
+
 using namespace std;
 
 int main() {
@@ -13,13 +14,20 @@ int main() {
 	image << width << " " << heigth << endl;
 	image << max_pixel << endl;
 	//end header
+	
 	//write body
+	//setting the seed to make it replicable if use random values to pixels :D
+	srand (42);
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < heigth; j++) {
 			image << i % max_pixel+1 << " " << j % max_pixel+1 << " " << (i + j) % max_pixel+1 << endl; // the 'mod' does not allow the value to exceed the value of max pixel
+			//uncomment the line below if generate random
+			//image << rand() % max_pixel + 1 << " " << rand() % max_pixel + 1 << " " << rand() % max_pixel + 1 << endl;
 		}
 	}
 	//end body
+
+
 	image.close(); //close file
 	return 0; //success
 }
